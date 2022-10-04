c6e997be49eb4e98a8202983c6bf08ffa51d9c76
Tran Dinh Cuong <kienthucnhatban.com@gmail.com>
2022-10-04 11:12:51 +0900

    Add model folder

---
+++ src/main/java/com/example/demoapp/model/model.java
@@ -0,0 +1,28 @@
+package com.example.demoapp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "crud")
+public class model {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "adress")
+    private String adress;
+
+}
